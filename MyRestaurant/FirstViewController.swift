d0b2cce200c1f22ea8d28914edd457107fb9d559
longweizou <1527842029@qq.com>
2020-05-25 16:30:11 +0800

    Initial Commit

---
+++ MyRestaurant/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MyRestaurant
+//
+//  Created by lw on 2020/5/25.
+//  Copyright © 2020 lw. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyRestaurant/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyRestaurant
+//
+//  Created by lw on 2020/5/25.
+//  Copyright © 2020 lw. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
